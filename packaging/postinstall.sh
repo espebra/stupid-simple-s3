276c85630892459cc43d5c5378905de2319dc10a
Espen Braastad <espen@varnish-software.com>
2026-01-18 22:54:44 +0100

    Include the environment file to make it easier to get started

--- packaging/postinstall.sh
+++ packaging/postinstall.sh
@@ -14,5 +14,8 @@ fi
 mkdir -p /var/lib/stupid-simple-s3/data /var/lib/stupid-simple-s3/tmp
 chown -R stupid:stupid /var/lib/stupid-simple-s3
 
+# Ensure config directory exists
+mkdir -p /etc/stupid-simple-s3
+
 # Reload systemd
 systemctl daemon-reload || true
